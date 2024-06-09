d7398ad8f3b02679508320a3d1e22e9b593e589d
Stanko <stanko@bezak.net>
2024-06-09 20:23:19 +0200

    catch error on segment removal

--- src/downloadCommand.ts
+++ src/downloadCommand.ts
@@ -14,182 +14,182 @@ import { Logger } from './logger';
 type DownloadType = 'hls' | 'file'
 
 export interface IDownloadCommand {
-    url: string
-    dest: string
-    name: string
-    state: 'complete' | 'pause' | 'error' | 'scheduled' | 'inProgress';
-    id: number
-    type: 'hls' | 'file'
-    error?: any
-    progress?: number
-    scrapeArgs?: showscrapeArgs | filmscrapeArgs
+  url: string
+  dest: string
+  name: string
+  state: 'complete' | 'pause' | 'error' | 'scheduled' | 'inProgress';
+  id: number
+  type: 'hls' | 'file'
+  error?: any
+  progress?: number
+  scrapeArgs?: showscrapeArgs | filmscrapeArgs
 }
 
 interface scrapeArgs {
-    source: string;
-    quality: Qualities;
+  source: string;
+  quality: Qualities;
 }
 
 interface showscrapeArgs extends scrapeArgs {
-    type: 'show';
-    data: MovieDB.Responses.TV.GetDetails;
-    season: number;
-    episode: number;
+  type: 'show';
+  data: MovieDB.Responses.TV.GetDetails;
+  season: number;
+  episode: number;
 }
 
 interface filmscrapeArgs extends scrapeArgs {
-    type: 'movie';
-    data: MovieDB.Responses.Movie.GetDetails;
+  type: 'movie';
+  data: MovieDB.Responses.Movie.GetDetails;
 }
 
 export async function Init() {
-    for (const key of Object.keys(downloaders)) {
-        delete downloaders[parseInt(key)];
+  for (const key of Object.keys(downloaders)) {
+    delete downloaders[parseInt(key)];
+  }
+  const downloads = await db.getAllDownloads()
+  for (const download of downloads) {
+    if (download.id == -1) continue;
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    const cmd = new DownloadCommand(download.url, download.dest, download.name, () => { }, {}, download.type, download.id)
+    if (download.scrapeArgs) {
+      cmd.scrapeArgs = download.scrapeArgs
     }
-    const downloads = await db.getAllDownloads()
-    for (const download of downloads) {
-        if(download.id == -1) continue;
-        // eslint-disable-next-line @typescript-eslint/no-empty-function
-        const cmd = new DownloadCommand(download.url, download.dest, download.name, ()=>{}, {}, download.type, download.id)
-        if(download.scrapeArgs){
-            cmd.scrapeArgs = download.scrapeArgs
-        }
-    }
-    console.log('Downloads Initialized');
+  }
+  console.log('Downloads Initialized');
 }
 
 export const downloaders: Record<number, DownloadCommand> = {}
 
 export default class DownloadCommand {
-    downloader: Downloader | undefined
-    _scrapeArgs: showscrapeArgs | filmscrapeArgs | undefined
+  downloader: Downloader | undefined
+  _scrapeArgs: showscrapeArgs | filmscrapeArgs | undefined
 
-    get scrapeArgs() {
-        return this._scrapeArgs
+  get scrapeArgs() {
+    return this._scrapeArgs
+  }
+
+  set scrapeArgs(val: showscrapeArgs | filmscrapeArgs | undefined) {
+    this._scrapeArgs = val
+    if (this.id != -1)
+      db.updateDownloadById(this.id, this.toJSON('scheduled'));
+    else setTimeout(() => {
+      db.updateDownloadById(this.id, this.toJSON('scheduled'));
+    }, 500)
+  }
+
+  constructor(private videoURL: string, private dest: string, private name: string, private cb: (success: boolean) => void, captionURLs: Record<string, string>, private type: DownloadType, private id: number = -1) {
+    if (!fs.existsSync(dest)) fs.mkdirSync(dest, { recursive: true, mode: 0o777 })
+    if (this.id == -1) {
+      db.addDownloadCommand(this.toJSON('scheduled')).then(id => {
+        this.id = id
+        console.log('New Download: ' + id)
+        this.init()
+      });
     }
+    else this.init()
+    this.downloadCaptions(captionURLs)
 
-    set scrapeArgs(val: showscrapeArgs | filmscrapeArgs | undefined) {
-        this._scrapeArgs = val
-        if(this.id != -1)
-            db.updateDownloadById(this.id, this.toJSON('scheduled'));
-        else setTimeout(()=> {
-            db.updateDownloadById(this.id, this.toJSON('scheduled'));
-        }, 500)
+  }
+
+  init() {
+    try {
+      const filepath = join(this.dest, this.name + extname(this.videoURL.split('?')[0]))
+      if (this.type == 'hls') {
+        this.downloader = new HlsDownloader(this.videoURL, filepath)
+      } else {
+        this.downloader = new FileDownloader(this.videoURL, filepath)
+      }
+      this.downloader.init().then((success) => {
+        this.cb(success)
+      })
+      downloaders[this.id] = this
+    } catch (e) {
+      this.cb(false)
     }
+  }
 
-    constructor(private videoURL: string, private dest: string, private name: string, private cb: (success: boolean) => void, captionURLs: Record<string, string>, private type: DownloadType, private id: number = -1) {
-        if(!fs.existsSync(dest)) fs.mkdirSync(dest, {recursive: true, mode: 0o777})
-        if(this.id == -1) {
-            db.addDownloadCommand(this.toJSON('scheduled')).then(id => {
-                this.id = id
-                console.log('New Download: ' + id)
-                this.init()
-            });
-        }
-        else this.init()
-        this.downloadCaptions(captionURLs)
-
+  async startDownload(retries = 0): Promise<boolean> {
+    if (retries > 5) {
+      db.updateDownloadById(this.id, { ...this.toJSON('error'), error: 'Failed to complete download after 5 retries' })
+      return false
     }
-
-    init() { 
-        try {
-            const filepath = join(this.dest, this.name + extname(this.videoURL.split('?')[0]))
-            if(this.type == 'hls'){
-                this.downloader = new HlsDownloader(this.videoURL, filepath)
-            } else {
-                this.downloader = new FileDownloader(this.videoURL, filepath)
-            }
-            this.downloader.init().then((success) => {
-                this.cb(success)
-            })
-            downloaders[this.id] = this
-        } catch (e) {
-            this.cb(false)
-        }
+    if (!this.downloader) return false
+    const state = (await db.getDownloadById(this.id)).state
+    if (state === 'scheduled') {
+      db.updateDownloadById(this.id, this.toJSON('inProgress'))
+      Logger.log(`Starting Download ${this.name}`);
+      this.downloader.start((p) => {
+        db.updateDownloadById(this.id, {
+          ...this.toJSON('inProgress'),
+          progress: p.percent
+        })
+      }).then(() => {
+        db.updateDownloadById(this.id, this.toJSON('complete'))
+      }).catch((err) => {
+        Logger.error(err);
+        db.updateDownloadById(this.id, this.toJSON('scheduled'));
+        this.reScrape().then(() => {
+          Logger.log("Re-scrape successfull");
+          this.init();
+          setTimeout(() => {
+            this.startDownload(retries + 1)
+          }, 100);
+        }).catch(err => {
+          db.updateDownloadById(this.id, { ...this.toJSON('error'), error: new Error('Error during re-scrape: ' + err) });
+          Logger.error(err);
+        })
+      })
     }
+    return false
+  }
 
-    async startDownload(retries = 0): Promise<boolean> {
-        if(retries > 5) {
-            db.updateDownloadById(this.id, {...this.toJSON('error'), error: 'Failed to complete download after 5 retries'})
-            return false
-        }
-        if(!this.downloader) return false
-        const state = (await db.getDownloadById(this.id)).state
-        if(state === 'scheduled'){
-            db.updateDownloadById(this.id, this.toJSON('inProgress'))
-            Logger.log(`Starting Download ${this.name}`);
-            this.downloader.start((p) => {
-                db.updateDownloadById(this.id, {
-                    ...this.toJSON('inProgress'),
-                    progress: p.percent
-                })
-            }).then(() => {
-                db.updateDownloadById(this.id, this.toJSON('complete'))
-            }).catch((err) => {
-                Logger.error(err);
-                db.updateDownloadById(this.id, this.toJSON('scheduled'));
-                this.reScrape().then(() => {
-                    Logger.log("Re-scrape successfull");
-                    this.init();
-                    setTimeout(() => {
-                        this.startDownload(retries + 1)
-                    }, 100);
-                }).catch(err=> {
-                    db.updateDownloadById(this.id, {...this.toJSON('error'), error: new Error('Error during re-scrape: ' + err) });
-                    Logger.error(err);
-                })
-            })
-        }
-        return false
+  async downloadCaptions(captionURLs: Record<string, string>) {
+    for (const caption of Object.keys(captionURLs)) {
+      const res = await axios.get(captionURLs[caption], { responseType: 'text' })
+      fs.writeFileSync(join(this.dest, this.name + '-' + caption + extname(captionURLs[caption])), res.data, { mode: 0o777 })
     }
+  }
 
-    async downloadCaptions(captionURLs: Record<string, string>) {
-        for (const caption of Object.keys(captionURLs)) {
-            const res = await axios.get(captionURLs[caption], {responseType: 'text'})
-            fs.writeFileSync(join(this.dest, this.name + '-' + caption + extname(captionURLs[caption])), res.data, {mode: 0o777})
-        }
-    }
+  cancel(): void {
+    this.downloader?.cancel()
+    db.updateDownloadById(this.id, this.toJSON('error'))
+  }
 
-    cancel(): void {
-        this.downloader?.cancel()
-        db.updateDownloadById(this.id, this.toJSON('error'))
+  toJSON(state: 'complete' | 'pause' | 'error' | 'scheduled' | 'inProgress'): IDownloadCommand {
+    return {
+      url: this.videoURL,
+      name: this.name,
+      dest: this.dest,
+      id: this.id,
+      type: this.type,
+      scrapeArgs: this.scrapeArgs,
+      state
     }
+  }
 
-    toJSON(state: 'complete' | 'pause' | 'error' | 'scheduled' | 'inProgress'): IDownloadCommand {
-        return {
-            url: this.videoURL,
-            name: this.name,
-            dest: this.dest,
-            id: this.id,
-            type: this.type,
-            scrapeArgs: this.scrapeArgs,
-            state
-        }
-    }
+  async reScrape() {
+    if (!this.scrapeArgs) throw new Error('No scrapeArgs');
+    let source: RunOutput | null;
+    if (this.scrapeArgs.type == 'movie') {
+      source = await ScrapeMovie(this.scrapeArgs.data, this.scrapeArgs.source)
+      if (!source) throw new Error("Source " + this.scrapeArgs.source + " hasn't got movie " + this.scrapeArgs.data.title);
 
-    async reScrape(){
-        if(!this.scrapeArgs) throw new Error('No scrapeArgs');
-        let source: RunOutput | null;
-        if(this.scrapeArgs.type == 'movie') {
-            source = await ScrapeMovie(this.scrapeArgs.data, this.scrapeArgs.source)
-            if(!source) throw new Error("Source " + this.scrapeArgs.source + " hasn't got movie " + this.scrapeArgs.data.title);
-            
-        } else {
-            source = await scrapeEpisode(this.scrapeArgs.data, this.scrapeArgs.season, this.scrapeArgs.episode, this.scrapeArgs.source);
-            if(!source) throw new Error("Source " + this.scrapeArgs.source + " hasn't got episode " + this.scrapeArgs.episode);
-        }
-        const stream = source.stream;
-        if(stream.type == 'file'){
-            const src = stream.qualities[this.scrapeArgs.quality]
-            if (!src) throw new Error('No source found for quality ' + this.scrapeArgs.quality);
-            this.videoURL = src.url
-        } else if(stream.type == 'hls') {
-            const playlist = await axios.get(stream.playlist, {responseType: 'text'}).catch(() => {throw new Error('Failed to get playlist')})
-            if(!playlist.data) throw new Error('Failed to get playlist')
-            const qualities = parseHlsQuality(playlist.data, stream.playlist)
-            const quality = qualities[this.scrapeArgs.quality]
-            if (!quality) throw new Error('No source found for quality ' + this.scrapeArgs.quality);
-            this.videoURL = quality
-        }
+    } else {
+      source = await scrapeEpisode(this.scrapeArgs.data, this.scrapeArgs.season, this.scrapeArgs.episode, this.scrapeArgs.source);
+      if (!source) throw new Error("Source " + this.scrapeArgs.source + " hasn't got episode " + this.scrapeArgs.episode);
     }
+    const stream = source.stream;
+    if (stream.type == 'file') {
+      const src = stream.qualities[this.scrapeArgs.quality]
+      if (!src) throw new Error('No source found for quality ' + this.scrapeArgs.quality);
+      this.videoURL = src.url
+    } else if (stream.type == 'hls') {
+      const playlist = await axios.get(stream.playlist, { responseType: 'text' }).catch(() => { throw new Error('Failed to get playlist') })
+      if (!playlist.data) throw new Error('Failed to get playlist')
+      const qualities = parseHlsQuality(playlist.data, stream.playlist)
+      const quality = qualities[this.scrapeArgs.quality]
+      if (!quality) throw new Error('No source found for quality ' + this.scrapeArgs.quality);
+      this.videoURL = quality
+    }
+  }
 }
--- src/downloaders/hls-download.ts
+++ src/downloaders/hls-download.ts
@@ -1,215 +1,219 @@
 import { spawn } from 'child_process';
 import path from 'path';
 import fs from 'fs/promises';
-import {existsSync, createReadStream, createWriteStream, WriteStream} from 'fs';
+import { existsSync, createReadStream, createWriteStream, WriteStream } from 'fs';
 import { ffprobe } from 'fluent-ffmpeg';
 import { DownloadProgress, Downloader } from '.';
 import axios from 'axios';
 import { Logger } from '../logger';
 
-export default class HlsDownloader extends Downloader  {
-    resumable = true
-    downloaded = 0
-    size = 0
-    queue: Promise<void>[] = []
-    segmentsDir = ''
-    constructor(protected url: string, protected filename: string) {
-        super(url, filename)
-    }
+export default class HlsDownloader extends Downloader {
+  resumable = true
+  downloaded = 0
+  size = 0
+  queue: Promise<void>[] = []
+  segmentsDir = ''
+  constructor(protected url: string, protected filename: string) {
+    super(url, filename)
+  }
 
-    async init(): Promise<boolean> {
-        const res = await axios.head(this.url, {
-            method: 'HEAD',
-            headers: {
-                Accept: '*/*',
-                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0'
-            }
-        }).catch(() => undefined)
-        if(!res) return false
-        const file = this.filename.split('/').pop()?.split('.')[0] || ''
-        const dir = this.filename.split('/').slice(0, -1).join('/')
-        this.segmentsDir = path.join(dir, 'segments-' + file.replaceAll(' ', '-'));
-        fs.mkdir(this.segmentsDir, {recursive: true});
-        return true
-    }
+  async init(): Promise<boolean> {
+    const res = await axios.head(this.url, {
+      method: 'HEAD',
+      headers: {
+        Accept: '*/*',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0'
+      }
+    }).catch(() => undefined)
+    if (!res) return false
+    const file = this.filename.split('/').pop()?.split('.')[0] || ''
+    const dir = this.filename.split('/').slice(0, -1).join('/')
+    this.segmentsDir = path.join(dir, 'segments-' + file.replaceAll(' ', '-'));
+    fs.mkdir(this.segmentsDir, { recursive: true });
+    return true
+  }
 
-    override async startDownload(progressCallback: (progress: DownloadProgress) => void): Promise<void> {
-        const file = this.filename.split('/').pop() || ''
-        const dir = this.filename.split('/').slice(0, -1).join('/')
-        const segments = await this.getSegments(this.url).catch(err => {
-            throw err
-        });
-        const baseUrl = this.url.split('/').slice(0, -1).join('/')
+  override async startDownload(progressCallback: (progress: DownloadProgress) => void): Promise<void> {
+    const file = this.filename.split('/').pop() || ''
+    const dir = this.filename.split('/').slice(0, -1).join('/')
+    const segments = await this.getSegments(this.url).catch(err => {
+      throw err
+    });
+    const baseUrl = this.url.split('/').slice(0, -1).join('/')
 
-        let done = 0
-        console.log(`downloading ${segments.length} segments`);
-        const paths: string[] = []
-        for (const segment of segments) {
-            const split = segment.split('/');
-            const p = path.join(this.segmentsDir, split[split.length - 1])
-            paths.push(p)
-            
-            if (existsSync(p) || HlsDownloader.filesProcessing.has(segment)) {
-                done++
-                if(HlsDownloader.filesProcessing.has(segment)) continue
-                progressCallback({
-                    downloaded: done,
-                    total: segments.length,
-                    percent: (done / segments.length) * 100
-                })
-                
-                continue
-            }
-            HlsDownloader.filesProcessing.add(segment);
-            this.queue.push(this.downloadSegment(baseUrl, segment, p).then(()=> {
-                done++
-                progressCallback({
-                    downloaded: done,
-                    total: segments.length,
-                    percent: (done / segments.length) * 100
-                })            
-            }).catch(err => {
-                throw err
-            }))
+    let done = 0
+    console.log(`downloading ${segments.length} segments`);
+    const paths: string[] = []
+    for (const segment of segments) {
+      const split = segment.split('/');
+      const p = path.join(this.segmentsDir, split[split.length - 1])
+      paths.push(p)
 
-            if (this.queue.length > 10) {
-                await Promise.all(this.queue)
-                this.queue.splice(0, this.queue.length)
-                await new Promise(r => setTimeout(r, 1000))
-            }
-        }
+      if (existsSync(p) || HlsDownloader.filesProcessing.has(segment)) {
+        done++
+        if (HlsDownloader.filesProcessing.has(segment)) continue
+        progressCallback({
+          downloaded: done,
+          total: segments.length,
+          percent: (done / segments.length) * 100
+        })
 
+        continue
+      }
+      HlsDownloader.filesProcessing.add(segment);
+      this.queue.push(this.downloadSegment(baseUrl, segment, p).then(() => {
+        done++
+        progressCallback({
+          downloaded: done,
+          total: segments.length,
+          percent: (done / segments.length) * 100
+        })
+      }).catch(err => {
+        throw err
+      }))
+
+      if (this.queue.length > 10) {
         await Promise.all(this.queue)
-
-        if (HlsDownloader.filesProcessing.has(file)) return
-        HlsDownloader.filesProcessing.add(file)
-        this.resumable = false
-        await this.mergeSegments(dir, file.split('.')[0], paths)
-        Logger.log(`Finished Download ${this.name}`)
+        this.queue.splice(0, this.queue.length)
+        await new Promise(r => setTimeout(r, 1000))
+      }
     }
 
-    async getSegments(playlistURL: string): Promise<string[]> {
-        const manifest = (await axios.get(playlistURL, {
-            responseType: 'text'
-        })).data
-        const out: string[] = []
-        const lines = manifest.split('\n')
-        for (let i = 0; i < lines.length; i++) {
-            const line = lines[i];
-            if(line.startsWith('#EXTINF:')){
-                const segment = lines[i+1]
-                out.push(segment)
+    await Promise.all(this.queue)
+
+    if (HlsDownloader.filesProcessing.has(file)) return
+    HlsDownloader.filesProcessing.add(file)
+    this.resumable = false
+    await this.mergeSegments(dir, file.split('.')[0], paths)
+    Logger.log(`Finished Download ${this.name}`)
+  }
+
+  async getSegments(playlistURL: string): Promise<string[]> {
+    const manifest = (await axios.get(playlistURL, {
+      responseType: 'text'
+    })).data
+    const out: string[] = []
+    const lines = manifest.split('\n')
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i];
+      if (line.startsWith('#EXTINF:')) {
+        const segment = lines[i + 1]
+        out.push(segment)
+      }
+    }
+
+    return out
+  }
+
+
+  override cancel(): void {
+    this.queue.forEach(e => e.catch(() => undefined))
+    this.queue = []
+  }
+
+
+  async fileToStream(file: string, out: WriteStream): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const stream = createReadStream(file)
+      stream.on('end', () => {
+        resolve()
+      })
+      stream.on('error', (err) => {
+        reject(err)
+      })
+      stream.pipe(out, { end: false })
+    })
+
+  }
+
+  async mergeSegments(dest: string, name: string, paths: string[]) {
+    name = name.replaceAll(':', '');
+    const stream = createWriteStream(name.replaceAll(':', '') + '.mp4')
+    for (const file of paths) {
+      await this.fileToStream(file, stream).catch(err => {
+        throw err
+      })
+    }
+    return new Promise<void>((resolve, reject) => {
+      stream.end(() => {
+        if (!existsSync(name + '.mp4')) reject('Failed to merge segments');
+        ffprobe(name + '.mp4', async (err, data) => {
+          if (err) reject(err)
+          const include: string[] = []
+          for (const stream of data.streams) {
+            if (stream.codec_type == 'video') {
+              include.push('-map', `0:${stream.index}`)
             }
-        }
-    
-        return out
-    }
-    
-
-    override cancel(): void {
-        this.queue.forEach(e => e.catch(()=>undefined))
-        this.queue = []
-    }
-
-
-    async fileToStream(file: string, out: WriteStream): Promise<void> {
-        return new Promise((resolve, reject) => {
-            const stream = createReadStream(file)
-            stream.on('end', () => {
-                resolve()
-            })
-            stream.on('error', (err) => {
-                reject(err)
-            })
-            stream.pipe(out, {end: false})
-        })
-        
-    }
-
-    async mergeSegments(dest: string, name: string, paths: string[]) {
-        name = name.replaceAll(':', '');
-        const stream = createWriteStream(name.replaceAll(':', '') + '.mp4')
-        for (const file of paths) {
-            await this.fileToStream(file, stream).catch(err => {
-                throw err
-            })
-        }
-        return new Promise<void>((resolve, reject) => {
-            stream.end(() => {  
-                if(!existsSync(name + '.mp4')) reject('Failed to merge segments');
-                ffprobe(name + '.mp4', async (err, data) => {
-                    if(err) reject(err)
-                    const include: string[] = []
-                    for (const stream of data.streams) {
-                        if (stream.codec_type == 'video') {
-                            include.push('-map', `0:${stream.index}`)
-                        }
-                        else if (stream.codec_type == 'audio') {
-                            include.push('-map', `0:${stream.index}`)
-                        }
-                    }
-                    const args = [
-                        '-y',
-                        '-i', `'./${name}.mp4'`,
-                        ...include,
-                        '-c', 'copy',
-                        `'./${name}.final.mp4'`
-                    ]
-                    const ffmpeg = spawn('ffmpeg', args, {cwd: process.cwd(), shell: true})
-                    ffmpeg.stderr.on('data', (data) => {
-                        Logger.warn('ffmpeg error: ' + data.toString());
-                    })
-                    ffmpeg.on('close', async (code) => {
-                        if (code != 0) {
-                            reject(code)
-                        }
-                        else {
-                            await fs.copyFile(`${name}.final.mp4`, `${dest}/${name}.mp4`);
-                            await fs.rm(`${name}.mp4`);
-                            await fs.rm(`${name}.final.mp4`);
-                            await this.removeSegments()
-                            resolve()
-                        }
-                    })
-                });
-            })
-    
-        })
-    }
-    
-
-    async removeSegments() {
-        const files = await fs.readdir(this.segmentsDir);
-        for (const file of files) {
-            await fs.rm(path.join(this.segmentsDir, file))
-        }
-
-        await fs.rmdir(this.segmentsDir)
-    }
-
-    async downloadSegment(url: string, segName: string, dest: string) {
-        
-        let retry = 0
-        
-        while (retry < 10) {
-            try {
-                let wholeURL = url + '/' + segName;
-                if(segName.startsWith('https://') || segName.startsWith('http://')){
-                    wholeURL = segName
-                }
-                const res = await axios.get(wholeURL, {responseType: 'arraybuffer', timeout: 4000})
-                const buf = res?.data                
-
-                if(buf){
-                    await fs.writeFile(dest, Buffer.from(buf))
-                    return
-                }
-                else throw new Error('No data')
-            } catch (error) {
-                retry++
-                continue
+            else if (stream.codec_type == 'audio') {
+              include.push('-map', `0:${stream.index}`)
             }
-        }
-        throw new Error('Failed to download ' + segName)
+          }
+          const args = [
+            '-y',
+            '-i', `'./${name}.mp4'`,
+            ...include,
+            '-c', 'copy',
+            `'./${name}.final.mp4'`
+          ]
+          const ffmpeg = spawn('ffmpeg', args, { cwd: process.cwd(), shell: true })
+          // ffmpeg.stderr.on('data', (data) => {
+          //   Logger.warn('ffmpeg error: ' + data.toString());
+          // })
+          ffmpeg.on('close', async (code) => {
+            if (code != 0) {
+              reject(code)
+            }
+            else {
+              await fs.copyFile(`${name}.final.mp4`, `${dest}/${name}.mp4`);
+              await fs.rm(`${name}.mp4`);
+              await fs.rm(`${name}.final.mp4`);
+              await this.removeSegments()
+              resolve()
+            }
+          })
+        });
+      })
+
+    })
+  }
+
+
+  async removeSegments() {
+    try {
+      const files = await fs.readdir(this.segmentsDir);
+      for (const file of files) {
+        await fs.rm(path.join(this.segmentsDir, file))
+      }
+
+      await fs.rmdir(this.segmentsDir)
+    } catch (error) {
+      Logger.warn('Failed to remove segments: ' + error)
     }
+  }
+
+  async downloadSegment(url: string, segName: string, dest: string) {
+
+    let retry = 0
+
+    while (retry < 10) {
+      try {
+        let wholeURL = url + '/' + segName;
+        if (segName.startsWith('https://') || segName.startsWith('http://')) {
+          wholeURL = segName
+        }
+        const res = await axios.get(wholeURL, { responseType: 'arraybuffer', timeout: 4000 })
+        const buf = res?.data
+
+        if (buf) {
+          await fs.writeFile(dest, Buffer.from(buf))
+          return
+        }
+        else throw new Error('No data')
+      } catch (error) {
+        retry++
+        continue
+      }
+    }
+    throw new Error('Failed to download ' + segName)
+  }
 }
